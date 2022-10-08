220b8edb36df3e706b0bd9f22e33468eba6801e3
misael <misael.geof@gmail.com>
2022-10-07 22:16:04 -0300

    add button at app

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 
-import { Input } from '../../components/Forms'
+import { Input } from '../../components/Forms/Input'
+import { Button } from '../../components/Forms/Button'
 
 import {
     Container,
     Header,
     Title,
     Form,
+    Fields,
 } from './styles'
 
 export function Register(){
@@ -18,12 +20,15 @@ export function Register(){
                 </Title>
             </Header>
             <Form>
-                <Input 
-                    placeholder="Nome"
-                />
-                <Input 
-                    placeholder="Preço"
-                />
+                <Fields>
+                    <Input 
+                        placeholder="Nome"
+                    />
+                    <Input 
+                        placeholder="Preço"
+                    />
+                </Fields>
+                <Button title="Enviar" />
             </Form>
         </Container>    
     )
