cf7eaf0b953631c196bb0d82dde15742e58cc783
misael <misael.geof@gmail.com>
2022-10-18 20:05:25 -0300

    feat: add transaction button and category

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Input } from '../../components/Forms/Input'
 import { Button } from '../../components/Forms/Button'
-
+import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton'
 import {
     Container,
     Header,
     Title,
     Form,
     Fields,
+    TransactionTypes,
 } from './styles'
+import { CategorySelect } from '../../components/Forms/CategorySelect'
 
 export function Register(){
+    const [transactionType, setTransactionType] = useState('')
+
+    const handleTransactionTypeSelect = (type: 'up' | 'down') => {
+        setTransactionType(type)
+    }
+
     return (
         <Container>
             <Header>
@@ -27,6 +35,21 @@ export function Register(){
                     <Input 
                         placeholder="Preço"
                     />
+                    <TransactionTypes>
+                        <TransactionTypeButton
+                            title='Income' 
+                            type='up'
+                            onPress={() => handleTransactionTypeSelect('up')}
+                            isActive={transactionType === 'up'}
+                            />
+                        <TransactionTypeButton 
+                            title='Outcome' 
+                            type='down'
+                            onPress={() => handleTransactionTypeSelect('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionTypes>
+                    <CategorySelect title="Categoria" />
                 </Fields>
                 <Button title="Enviar" />
             </Form>
